9c24f5a623311fe263ef27e9ef972ff5bcd87384
idealer <2755027635@qq.com>
2024-03-16 17:29:42 +0800

    解决跨域问题与前后端交互测试

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,22 +3,23 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo(){
-        List<String> strings = new LinkedList<>();
+    public Map<String,String> getBotInfo(){
+        Map<String, String> map = new HashMap<>();
 
-        strings.add("a");
-        strings.add("b");
-        strings.add("c");
+        map.put("s1","1000");
+        map.put("s2","2000");
 
-        return strings;
+        return map;
     }
 
 }
