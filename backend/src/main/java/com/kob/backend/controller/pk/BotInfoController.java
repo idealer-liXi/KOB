a6434a7bd67ec7a66d0e7ca9d6999aaf488d5681
idealer <2755027635@qq.com>
2024-03-16 15:28:46 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,24 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getbotinfo/")
+    public List<String> getBotInfo(){
+        List<String> strings = new LinkedList<>();
+
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+
+        return strings;
+    }
+
+}
